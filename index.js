a953bf49cb271d80e170ebd290b1c4e621edeee8
Diego Richard Lemos <10diieggos@gmail.com>
2020-08-16 16:15:34 -0300

    Aula 149 - Tratamento de erros em callbacks

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
+/* Quando usamos callback, as falhas são tratadas com parâmetros */
 function sendEmail(body, To, callback) {
   setTimeout(() => {
-    console.log(`
-    Para: ${To}
-    ----------------------------
-    ${body}
-    ----------------------------
-    `)
-    callback('sucesso', 5, '3 segundos');
+    //lógica do envio do e-mail...
+    let error = true;
+    
+    if (error) {
+      callback(3, 'Falhou');      
+    } else {
+      callback(3, 'Enviado com sucesso');
+    }
   }, 3000);
 }
-
-let successMessage = (status, amount, time) => {
-  console.log(`
-  Enviado com ${status}
-  ---
-  para ${amount} destinatários
-  ---
-  após ${time}
-  `)
+//successMessage é uma função que é passada como parâmetro na chamada da função sendEmail
+let successMessage = (time, error) => {
+  console.log(`${error} após ${time} seg`)
 };
 
-console.log('Enviando e-mail...');
 sendEmail("Seja bem vindo ao IAmDev", "Macielly", successMessage)
+console.log('Enviando e-mail...');//esta menssagem é assincrona. Ela é executada antes do término da execução de sendEmail
 
