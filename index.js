bc45748ad2708fdc39b8c220c3292e0599b4e9b2
Diego Richard Lemos <10diieggos@gmail.com>
2020-08-17 18:54:47 -0300

    Aula 154 - Async/Await

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
-function takeUser() {
+function takeUsers() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({id: 5});
-    }, 2000);
+    resolve([{ name: 'Diego', lang: 'JS' },
+      { name: 'Richard', lang: 'Python' },
+      { name: 'Lemos', lang: 'PHP' }
+    ]);
   });
 };
 
-function checkUser(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({to: '10diieggos@gmail.com'})
-    }, 2000);
-  });
-};
+async function principal() {
+  let users = await takeUsers()
+  console.log(users)
+  console.log('Esta foi bloqueada até o fim da principal')
+}
 
-function sendEmail(body) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let error = true;
-      if (error) {
-        reject("A fila está cheia, envio falhou");
-      } else {
-        resolve({time: 7, body});
-      };
-    }, 3000);
-  });
-};
+principal()
 
-takeUser().then(({ id }) => {
-  checkUser(id).then(({ to }) => {
-    sendEmail('Parabéns, você conseguiu', to).then(({ time, body }) => {
-      console.log(`
-      Enviado após ${time} seg.
-      -------
-      Para: ${to}
-      -------
-      Mensagem:
-      ${body}
-      -------
-      Id do usuário: ${id}
-      `);
-    }).catch((error) => {console.log(error)})
-  })
-});
-
-console.log('Enviando email...')
+console.log('Esta mensagem não foi bloqueada')
